d1f6917044d569884fa8045b770d4363cd20e7c7
Joseph <91016054+Joseph-ink@users.noreply.github.com>
2023-05-24 12:23:28 +0800

    Update install_tuic.sh

--- install_tuic.sh
+++ install_tuic.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# 移除之前的安装
+systemctl stop tuic
+rm /etc/systemd/system/tuic.service
+rm -rf /etc/tuic
+
 # 创建目录
-mkdir -p /root/tuic
+mkdir -p /etc/tuic
 
 # 自动匹配系统架构并下载二进制程序
 if [[ "$(uname -m)" == "x86_64" ]]; then
@@ -9,11 +14,11 @@ if [[ "$(uname -m)" == "x86_64" ]]; then
 else
   ARCH="aarch64"
 fi
-curl -Lo /root/tuic/tuic https://github.com/EAimTY/tuic/releases/download/0.8.5/tuic-server-0.8.5-${ARCH}-linux-gnu
-chmod +x /root/tuic/tuic
+curl -Lo /etc/tuic/tuic https://github.com/EAimTY/tuic/releases/download/0.8.5/tuic-server-0.8.5-${ARCH}-linux-gnu
+chmod +x /etc/tuic/tuic
 
 # 下载配置文件
-curl -Lo /root/tuic/tuic_config.json https://raw.githubusercontent.com/Joseph-ink/tuic-install/main/config_server.json
+curl -Lo /etc/tuic/tuic_config.json https://raw.githubusercontent.com/Joseph-ink/tuic-install/main/config_server.json
 
 # 提示用户输入配置参数
 read -p "请输入tuic端口号（默认16386）：" PORT
@@ -56,10 +61,10 @@ case $CERT_CHOICE in
 esac
 
 # 替换配置文件中的参数
-sed -i "s/\"port\": 16386/\"port\": ${PORT}/g" /root/tuic/tuic_config.json
-sed -i "s/\"token\": \[\"chika\"\]/\"token\": [\"${TOKEN}\"]/g" /root/tuic/tuic_config.json
-sed -i "s|\"certificate\": \"/root/cert/cert.crt\"|\"certificate\": \"${CERT}\"|g" /root/tuic/tuic_config.json
-sed -i "s|\"private_key\": \"/root/cert/private.key\"|\"private_key\": \"${PRIV_KEY}\"|g" /root/tuic/tuic_config.json
+sed -i "s/\"port\": 16386/\"port\": ${PORT}/g" /etc/tuic/tuic_config.json
+sed -i "s/\"token\": \[\"chika\"\]/\"token\": [\"${TOKEN}\"]/g" /etc/tuic/tuic_config.json
+sed -i "s|\"certificate\": \"/root/cert/cert.crt\"|\"certificate\": \"${CERT}\"|g" /etc/tuic/tuic_config.json
+sed -i "s|\"private_key\": \"/root/cert/private.key\"|\"private_key\": \"${PRIV_KEY}\"|g" /etc/tuic/tuic_config.json
 
 # 下载systemctl配置
 curl -Lo /etc/systemd/system/tuic.service https://raw.githubusercontent.com/Joseph-ink/tuic-install/main/tuic.service
